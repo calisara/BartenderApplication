847c03d3d42201bfe7e78a8ed08b5a7f1dbd552e
Sara Cali <s.cali0104@gmail.com>
2021-08-31 23:19:09 -0400

    Added the tickets from customers, set up customer view.

--- BartenderApplication/Controllers/CustomerController.cs
+++ BartenderApplication/Controllers/CustomerController.cs
@@ -3,14 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using BartenderApplication.Models;
+using BartenderApplication.Repository;
+
 
 namespace BartenderApplication.Controllers
 {
+    [Authorize("Customer")]
     public class CustomerController : Controller
     {
+        private FakeRepository _fakeRepository;
+
+        public CustomerController(FakeRepository fakeRepository)
+        {
+            _fakeRepository = fakeRepository;
+        }
+
         public IActionResult Index()
         {
-            return View("~/Views/Home/Customer.cshtml");
+            IEnumerable<DrinksModel> drinksmodel = _fakeRepository.Drinks;
+            return View("~/Views/Home/Customer", drinksmodel.ToList());
         }
+
+        public IActionResult CreateTicket(TicketsModel ticketsModel)
+        {
+            ticketsModel.Id = Guid.NewGuid().ToString();
+            _fakeRepository.Tickets.Add(ticketsModel);
+            return RedirectToAction("Index");
+        }
+
+        
     }
 }
